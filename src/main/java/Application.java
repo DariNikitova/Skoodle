cd955eeb9097635e3baa209e5407f10a5b75be26
Ivan Milev <ivanmilevTUES@gmail.com>
2017-04-12 19:30:57 +0300

    Rooms

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,13 @@
-package PACKAGE_NAME;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by ivan on 12.4.2017 г..
  */
+
+@SpringBootApplication
 public class Application {
+    public static void main(String [] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,7 +1,43 @@
-package PACKAGE_NAME;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * Created by ivan on 12.4.2017 г..
  */
+
+@Entity
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String topic;
+    private long maxPlayers;
+    private long[] playerIds;
+
+    public long getId() {
+        return id;
+    }
+
+    public long getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public void setMaxPlayers(long maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public long[] getPlayerIds() {
+        return playerIds;
+    }
 }
