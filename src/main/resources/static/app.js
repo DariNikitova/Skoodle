e59fff913b9dc914a682229d13d15aa6f7e15a54
Ivan Milev <ivanmilevTUES@gmail.com>
2017-04-20 20:14:49 +0300

    JS file added

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -12,6 +12,9 @@ function setConnected(connected) {
     $("#greetings").html("");
 }
 
+$.ajax({url: "http://localhost:8080/rooms", success: function(result){
+        alert(result);
+}});
 
 window.onload = function connect() {
     var socket = new SockJS('/gs-guide-websocket');
@@ -25,15 +28,6 @@ window.onload = function connect() {
     });
 }
 
-// For now we don't need this
-//function disconnect() {
-//    if (stompClient != null) {
-//        stompClient.disconnect();
-//    }
-//    setConnected(false);
-//    console.log("Disconnected");
-//}
-
 function sendName() {
     stompClient.send("/app/hello", {}, JSON.stringify({'maxPlayers': $("#maxPlayers").val()}));
 }
@@ -43,8 +37,6 @@ function showRoom(room) {
 }
 
 
-
-// We don't need this for now :)
 $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
@@ -53,15 +45,3 @@ $(function () {
     $( "#disconnect" ).click(function() { disconnect(); });
     $( "#send" ).click(function() { sendName(); });
 });
-
-
-// Ask tisho about the Rest thing cause you can't still deal with it.
-//window.onload = function() {
-//    $.ajax({
-//        'utl': '/',
-//        'type': 'GET',
-//        'success' : function(data) {
-//            alert("DATA REQUESTEd!")
-//        }
-//    })
-//}
